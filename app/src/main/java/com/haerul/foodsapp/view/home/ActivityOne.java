d385b755065f9e030e7ff0792e37046639e827bb
habao132 <habao132@gmail.com>
2020-05-19 14:52:58 +0700

    first commit

---
+++ app/src/main/java/com/haerul/foodsapp/view/home/ActivityOne.java
@@ -0,0 +1,21 @@
+/*-----------------------------------------------------------------------------
+ - Developed by Haerul Muttaqin                                               -
+ - Last modified 5/18/20 10:12 AM                                             -
+ - Subscribe : https://www.youtube.com/haerulmuttaqin                         -
+ - Copyright (c) 2020. All rights reserved                                    -
+ -----------------------------------------------------------------------------*/
+package com.haerul.foodsapp.view.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.haerul.foodsapp.R;
+
+public class ActivityOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+    }
+}
---
+++ app/src/main/java/com/haerul/foodsapp/view/home/ActivityTwo.java
@@ -0,0 +1,21 @@
+/*-----------------------------------------------------------------------------
+ - Developed by Haerul Muttaqin                                               -
+ - Last modified 5/17/20 10:15 PM                                             -
+ - Subscribe : https://www.youtube.com/haerulmuttaqin                         -
+ - Copyright (c) 2020. All rights reserved                                    -
+ -----------------------------------------------------------------------------*/
+package com.haerul.foodsapp.view.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.haerul.foodsapp.R;
+
+public class ActivityTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+    }
+}
